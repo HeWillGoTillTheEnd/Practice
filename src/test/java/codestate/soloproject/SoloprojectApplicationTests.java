24a7c40fd2aae1b3dd2d9815de64fd3487935b97
SaintWater <40792144+IWillGoTillTheEnd@users.noreply.github.com>
2023-04-12 14:40:35 +0900

    update

---
+++ src/test/java/codestate/soloproject/SoloprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package codestate.soloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
